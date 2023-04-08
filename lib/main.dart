1958c25fc6f7a660604e45faf2bd64251dddd6ba
tin <phantrungtin01@gmail.com>
2023-04-09 02:14:55 +0700

    Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pilates Community',
+      title: 'BLoC Architecture',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey.shade200,
         primarySwatch: Colors.blue,
